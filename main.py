b7fdb6508e755a9683c1460a4335835a00f45050
tunedcoder <thetunedcoder@gmail.com>
2021-05-21 10:20:10 +0530

    added some code for smtp and Join

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import discord
 import os
-import struct
+from dotenv import load_dotenv
 import numpy
 import pandas as pd
-import xlutils
+import smtplib
+from openpyxl import workbook
+
+def send_mail(to,body)
+    server = smtplib.SMTP_SSL("smtp.gmail.com",465)
+    server.login("examples@gmail.com", "password")
+    server.sendmail("from", "to" , body)
+
+def df_init():
+    if os.path.exists('maind.csv'):
+        return
+    else:
+        headers = ["did", "vcode", "Name", "Roll No.", "Branch", "Year", "Roles"]
+        df = pd.DataFrame(columns=headers)
+        df.to_csv('maind.csv')
+        return
+
+def verify_user(did):
 
 
-
+def new_user(did):
+    pass
 
 class Bot:
     pass
@@ -16,10 +34,10 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print("Hi, {0}".format(name))  # Press Ctrl+F8 to toggle the breakpoint.
 
-info = ["Name","Roll No.", "Hobbies", "Branch"]
+info = ["Name", "Roll No.", "Hobbies", "Branch"]
 
 def main():
-    BOT_TOKEN = "ODE1NjU2NDkwODI3Nzc2MDIw.YDvlTQ.E0i0XwumRa-CHYviHXjEkKJbD3E"
+    #BOT_TOKEN = "ODE1NjU2NDkwODI3Nzc2MDIw.YDvlTQ.E0i0XwumRa-CHYviHXjEkKJbD3E"
     client=discord.Client()
 
     @client.event
@@ -38,6 +56,16 @@ def main():
             if message.content.startswith('$reg'):
                 await message.channel.send('Registration Starts')
                 for field in info:
+                    if field == "Roll No.":
+                        await message.channel.send(f'Enter {field}')
+
+                        def check(m):
+                            return m.author != client.user and m.content == hash(did)
+
+                        name = await client.wait_for('message', check=check)
+                        out = name.content
+
+                        await message.channel.send(out)
                     await message.channel.send(f'Enter {field}')
                     def check(m):
                         return m.author != client.user and m.content != None
@@ -48,10 +76,15 @@ def main():
     @client.event
     async def on_member_join(member):
         did = member.id
+        df2 = pd.DataFrame([did,hash(did),"","","","",[]])
+        dfm.append(df2)
 
-        
-    client.run(BOT_TOKEN)
+    client.run(TOKEN)
 
 if __name__ == '__main__':
+    load_dotenv()
+    TOKEN = os.getenv("B_TOK")
+    df_init()
+    dfm = pd.read_csv("maind.csv")
     main()
 
